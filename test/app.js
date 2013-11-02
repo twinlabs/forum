29e2335a8d4505994070b5f0856b7ab5cc2333a6
Noah Chase <nchase@gmail.com>
2013-11-01 21:28:29 -0400

    add a way to test main app functionality in the future

--- test/app.js
+++ test/app.js
@@ -1,7 +1,10 @@
 var assert = require('assert');
+var sinon = require('sinon');
+var app = require('./app');
 
-describe('does a thing', function(){
-  it('is truthful, always', function(){
-    assert.equal(false, true);
+describe('app.js', function(){
+  xit('accepts port as the first command-line argument', function(){
+    sinon.spy(console, 'log');
+    assert.ok(console.log.calledWith('listening on port 3000'));
   });
 });
