4682fa2c81f56ade79d7841183da39e7a4b8522d
Noah Chase <nchase@gmail.com>
2013-12-25 11:00:21 -0500

    separate post 'fixture' data from 'sandboxed' post template. use moment JS to get _real_ relative times

--- app.js
+++ app.js
@@ -40,6 +40,30 @@ app.get('/uisandbox', function(request, response){
   response.render('posts/show');
 });
 
+app.locals({
+  posts: [
+    {
+      id: 'post_1',
+      date: moment('Wed Dec 25 2013 7:13:52 GMT-0500 (EST)').fromNow(),
+      user: 'Willis',
+      body: "There was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the Rabbit say to itself, 'Oh dear! Oh dear! I shall be late!' (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge."
+    },
+    {
+      id: 'post_2',
+      date: moment('Wed Dec 25 2013 9:13:52 GMT-0500 (EST)').fromNow(),
+      user: 'David',
+      body: "So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her."
+
+    },
+    {
+      id: 'post_3',
+      date: moment('Wed Dec 25 2013 10:13:52 GMT-0500 (EST)').fromNow(),
+      user: 'Alice',
+      body: "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?<br/><img src=\"http://m.ahfr.org/content/cover3.gif\"/>"
+    }
+  ]
+});
+
 
 var server = app.listen(PORT);
 console.log('listening on port ' + PORT);
