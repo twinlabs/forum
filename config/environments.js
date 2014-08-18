caecdc22d1954170880bc1a25f309b976a0091d6
Noah Chase <nchase@gmail.com>
2014-08-18 02:15:06 -0400

    slim down app.js:
    * introduce routes.js for all of the routing stuff we were doing in app.js before
    * move some default setup to environments.js
    * move the socket io server to a property on the `app` object
    * miscellaneous extractions (mainly there was just too much going on in app.js.

--- config/environments.js
+++ config/environments.js
@@ -1,5 +1,13 @@
+var argv = require('optimist').argv;
+
 module.exports = function(app){
-  app.configure('development', function(){
+  app.configure(function(){
     app.set('hostName', 'http://localhost');
+    app.set('PORT', argv.port || 3000);
+    app.set('SOCKET_PORT', argv.socket_port || 5000);
+  });
+
+  app.configure('development', function(){
+    // dev environment-specific stuff here:
   });
 };
---
+++ config/routes.js
@@ -0,0 +1,27 @@
+var PostsController = rootRequire('app/controllers/PostsController');
+
+var routes = function(app){
+  app.get('/', function(request, response){
+    PostsController.index().done(function(error, posts){
+      response.render('index', {
+        posts: posts
+      });
+    });
+  });
+
+  app.get('io').sockets.on('connection', function(socket){
+    socket.on('post', function(data){
+
+      PostsController.add(data);
+      app.get('io').sockets.emit('post', data);
+    });
+  });
+
+  app.get('/uisandbox', function(request, response){
+    response.render('posts/show', {
+      posts: rootRequire('test/fixtures/posts')
+    });
+  });
+};
+
+module.exports = routes;
