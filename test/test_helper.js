dbf632b188aa3242ac2d0f1a360f61526b771ae7
Noah Chase <nchase@gmail.com>
2014-08-22 21:08:31 -0400

    use app-level helpers in test_helper module

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,2 @@
-var path = require('path');
-var rootDir = __dirname + '/../';
+require('../lib/helpers');
 
-module.exports = {
-  rootRequire: function(modulePath){ return require(rootDir + modulePath); },
-  rootDir: path.resolve(rootDir)
-};
-
-global.rootRequire = module.exports.rootRequire;
-global.rootDir = module.exports.rootDir;
