3b5028a9cddbd542dde51bb392216d83fa50d98c
Noah Chase <nchase@gmail.com>
2014-08-24 21:03:12 -0400

    34: introduce a failing signup test

---
+++ test/integration/signup.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+var appModule = rootRequire('app');
+
+describe ('signup', function(){
+  it('accepts signup requests with a username and password and creates new users', function(){
+    assert(false);
+  });
+});
