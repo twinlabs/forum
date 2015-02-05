0fe9a4d05c67a7c2d33bc4cabe9f935913f22e0a
Noah Chase <nchase@gmail.com>
2015-02-05 13:06:40 -0500

    update sequelize-related tests to use 2.0.x API.

--- test/unit/app/associations/UserHasPosts.js
+++ test/unit/app/associations/UserHasPosts.js
@@ -1,26 +1,26 @@
 var assert = require('assert');
-var Post = rootRequire('app/models/Post');
-var User = rootRequire('app/models/User');
+var post = rootRequire('app/models/Post');
+var user = rootRequire('app/models/User');
 
 var models = {
-  Post: Post,
-  User: User
+  post: post,
+  user: user
 };
 
 
 describe('User', function(){
   before(function(){
-    Post.associate(models);
-    User.associate(models);
+    post.associate(models);
+    user.associate(models);
   });
 
   it('can ask for posts', function(){
-    var user = User.build({
+    var userInstance = user.build({
       name: 'Ty',
       id: 1
     });
 
-    assert(typeof user.getPosts === "function");
+    assert(typeof userInstance.getPosts === "function");
   });
 
   xit('gets posts associated with a particular user', function(){
