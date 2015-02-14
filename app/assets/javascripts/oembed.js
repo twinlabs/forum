2c3a636ec13c30897e848c2ea234febbaf11e88d
Noah Chase <nchase@gmail.com>
2015-02-14 15:33:35 -0500

    rough cut at 'oembed' for twitter.

---
+++ app/assets/javascripts/oembed.js
@@ -0,0 +1,32 @@
+$(function() {
+  embedTwitter($('.content a'));
+});
+
+function embedTwitter($selector) {
+  var TWITTER = /https?:\/\/(www\.)?twitter.com\/.+?\/status(es)?/i;
+
+  $selector.filter(function(index, element) {
+    var match = false;
+
+    if (this.getAttribute('href').match(TWITTER) !== null) {
+      match = true;
+    }
+
+    if ($(this).parent('.twitter-tweet').length) {
+      match = false;
+    }
+
+    return match;
+  }).each(function(index, element) {
+    $.ajax({
+      url: 'https://api.twitter.com/1/statuses/oembed.json?url=' + encodeURIComponent(element.getAttribute('href')),
+      dataType: 'jsonp',
+      headers: {
+        'Authorization': 'OAuth oauth_consumer_key="nujH4OgzdEtr9RcA55iOaYpzp", oauth_nonce="d953a64aad3116c9c6f66f78a2bc0aa6", oauth_signature="jZ5UPRi1Pz3xX1XfgMR6vUXfVLA%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1423944834", oauth_version="1.0"'
+      },
+      success: function(data) {
+        $(element).replaceWith(data.html);
+      }
+    });
+  });
+}
