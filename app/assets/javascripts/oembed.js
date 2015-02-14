0f7d11f828b36c8cdec6904568b8923c32de3e76
Noah Chase <nchase@gmail.com>
2015-02-14 18:04:20 -0500

    handle mobile twitter URLs too.

--- app/assets/javascripts/oembed.js
+++ app/assets/javascripts/oembed.js
@@ -3,7 +3,7 @@ $(function() {
 });
 
 function embedTwitter($selector) {
-  var TWITTER = /https?:\/\/(www\.)?twitter.com\/.+?\/status(es)?/i;
+  var TWITTER = /https?:\/\/(www\.)?(mobile\.)?twitter.com\/.+?\/status(es)?/i;
 
   $selector.filter(function(index, element) {
     var match = false;
@@ -18,8 +18,12 @@ function embedTwitter($selector) {
 
     return match;
   }).each(function(index, element) {
+    var requestUrl = element.getAttribute('href');
+
+    requestUrl = requestUrl.replace('mobile.', '');
+
     $.ajax({
-      url: 'https://api.twitter.com/1/statuses/oembed.json?url=' + encodeURIComponent(element.getAttribute('href')),
+      url: 'https://api.twitter.com/1/statuses/oembed.json?url=' + encodeURIComponent(requestUrl),
       dataType: 'jsonp',
       headers: {
         'Authorization': 'OAuth oauth_consumer_key="nujH4OgzdEtr9RcA55iOaYpzp", oauth_nonce="d953a64aad3116c9c6f66f78a2bc0aa6", oauth_signature="jZ5UPRi1Pz3xX1XfgMR6vUXfVLA%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1423944834", oauth_version="1.0"'
