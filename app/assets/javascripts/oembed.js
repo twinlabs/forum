390a3d05708542b0a952595548ee7d140c66483e
Noah Chase <nchase@gmail.com>
2015-02-15 18:32:32 -0500

    add instagram URL handler

--- app/assets/javascripts/oembed.js
+++ app/assets/javascripts/oembed.js
@@ -1,5 +1,6 @@
 $(function() {
   embedTwitter($('.content a'));
+  embedInstagram($('.content a'));
 });
 
 function embedTwitter($selector) {
@@ -28,6 +29,36 @@ function embedTwitter($selector) {
       headers: {
         'Authorization': 'OAuth oauth_consumer_key="nujH4OgzdEtr9RcA55iOaYpzp", oauth_nonce="d953a64aad3116c9c6f66f78a2bc0aa6", oauth_signature="jZ5UPRi1Pz3xX1XfgMR6vUXfVLA%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1423944834", oauth_version="1.0"'
       },
+      success: function(data) {
+        $(element).replaceWith(data.html);
+      }
+    });
+  });
+}
+
+function embedInstagram($selector) {
+  var INSTAGRAM = /https?:\/\/(www\.)?(instagram\.com|instagr\.am)\/p\/.*/i;
+
+  $selector.filter(function(index, element) {
+    var match = false;
+
+    if (this.getAttribute('href').match(INSTAGRAM) !== null) {
+      match = true;
+    }
+
+    if ($(this).closest('.instagram-media').length) {
+      match = false;
+    }
+
+    return match;
+  }).each(function(index, element) {
+    var requestUrl = element.getAttribute('href');
+
+    requestUrl = requestUrl.replace('mobile.', '');
+
+    $.ajax({
+      url: 'https://api.instagram.com/oembed?url=' + encodeURIComponent(requestUrl),
+      dataType: 'jsonp',
       success: function(data) {
         $(element).replaceWith(data.html);
       }
