3c1ee451443c4e485b376abac8f2e7a9e9342e1d
Noah Chase <nchase@gmail.com>
2015-03-28 19:35:49 -0400

    use express 4 style config (i.e. just look at process.env.NODE_ENV)

--- config/environments.js
+++ config/environments.js
@@ -1,26 +1,22 @@
 module.exports = function(app){
-  app.configure(function(){
-    app.set('hostName', 'http://localhost');
-    app.set('PORT', process.env.PORT || 3000);
-    app.set('db-test', 'forum_test');
-    app.set('signupToken', 'c4d256875135311a8da112a57da39d7d');
-  });
+  app.set('hostName', 'http://localhost');
+  app.set('PORT', process.env.PORT || 3000);
+  app.set('db-test', 'forum_test');
+  app.set('signupToken', 'c4d256875135311a8da112a57da39d7d');
 
-  app.configure('development', function(){
-    // dev environment-specific stuff here:
-  });
 
-  app.configure('test', function(){
+  if (process.env.NODE_ENV === 'test') {
     // test environment-specific stuff here:
     app.set('PORT', process.env.PORT || 3099);
-  });
+  }
 
 
-  app.configure('production', function(){
+  if (process.env.NODE_ENV === 'production') {
+    // production environment-specific stuff here:
     var environment = rootRequire('config/environments/production');
 
     app.set('hostName', environment.hostName);
     app.set('sessionSecret', environment.sessionSecret);
     app.set('signupToken', environment.signupToken);
-  });
+  }
 };
