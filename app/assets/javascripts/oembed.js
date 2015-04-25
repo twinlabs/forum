586c01b0706df3630b234b4415522bf53020f97e
Noah Chase <nchase@gmail.com>
2015-04-25 18:15:21 -0400

    add vine auto-embed support

--- app/assets/javascripts/oembed.js
+++ app/assets/javascripts/oembed.js
@@ -3,6 +3,7 @@ $(function() {
   embedInstagram($('.content a'));
   embedYouTube($('.content a'));
   embedSoundCloud($('.content a'));
+  embedVine($('.content a'));
 });
 
 function embedTwitter($selector) {
@@ -112,5 +113,23 @@ function embedSoundCloud($selector) {
         $(element).replaceWith(data.html);
       }
     });
+  });
+}
+
+function embedVine($selector) {
+  var VINE = /(?:https?:\/\/)vine.co\/v\/(.+)/;
+
+  $selector.filter(function(index, element) {
+    var match = false;
+
+    if (this.getAttribute('href').match(VINE) !== null) {
+      match = true;
+    }
+
+    return match;
+  }).each(function(index, element) {
+    var matchID = element.getAttribute('href').match(VINE)[1];
+
+    $(element).replaceWith('<iframe src="https://www.vine.co/v/'  + matchID + '/embed/postcard"' +  ' width="600" height="600" frameborder="0"></iframe><script src="https://platform.vine.co/static/scripts/embed.js"></script>');
   });
 }
