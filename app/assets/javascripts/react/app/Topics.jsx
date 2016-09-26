0e146b7551f95bc06254bab4258620f1b96482d3
Noah Chase <nchase@gmail.com>
2016-09-26 00:54:43 -0400

    add basic filter component

--- app/assets/javascripts/react/app/Topics.jsx
+++ app/assets/javascripts/react/app/Topics.jsx
@@ -4,12 +4,36 @@ var ReactDOM = require('react-dom');
 var Topic = require('./Topic.jsx');
 
 var Topics = React.createClass({
+  getInitialState: function() {
+    return {
+      filterValue: null
+    };
+  },
+
+  updateFilterValue: function(event) {
+    this.setState({
+      filterValue: event.target.value
+    });
+  },
+
   render: function() {
     return (
       <div className="topicsContainer">
+        <div className="topicsFilter">
+          <input
+            className="input"
+            type="text"
+            placeholder="Filter Topic Name"
+            ref="topicFilter"
+            onChange={this.updateFilterValue}
+          />
+        </div>
         {_.filter(this.props.value.topics, function(topic) {
+          if (this.state.filterValue) {
+            return !topic.parent && (topic.title.toLowerCase().indexOf(this.state.filterValue.toLowerCase()) !== -1);
+          }
           return !topic.parent;
-         }).map(Topic)}
+         }.bind(this)).map(Topic)}
       </div>
     );
   }
