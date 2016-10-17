a7342369b8701c4cfba590f99f17501fbbab52a3
Noah Chase <nchase@gmail.com>
2016-10-17 19:25:38 -0400

    better youtube embeds and a rudimentary test

---
+++ test/unit/embed/youtube.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var embedYouTube = require('../../../v2/app/components/oembed/oembed-youtube');
+var superagent = require('superagent');
+var sinon = require('sinon');
+var dedent = require('dedent');
+
+describe('youtube embeds', function() {
+  it('correctly produces frames for basic youtube URLs', function(done) {
+    embedYouTube('https://www.youtube.com/watch?v=RD_44xEEDjU').then(function(output) {
+      console.log('call done', output);
+      var expectedOutput = `<iframe style="min-height: 350px" src="//www.youtube.com/embed/RD_44xEEDjU" frameborder="0" allowfullscreen></iframe>`;
+
+      assert.equal(output, expectedOutput);
+
+      done();
+    });
+  });
+});
--- v2/app/components/oembed/oembed-youtube.js
+++ v2/app/components/oembed/oembed-youtube.js
@@ -1,13 +1,25 @@
 module.exports = function embedYouTube(input) {
-  const YOUTUBE = /(?:https?:\/\/)?(?:m\.)?(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/;
+  const YOUTUBE = /(?:https?:\/\/)?(?:m\.)?(?:www\.)?(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
 
-  if (!input || input.match(YOUTUBE) === null) {
+  var matches = input && input.match(new RegExp(YOUTUBE, 'g'));
+  var matchID;
+  var responseBody = '';
+
+  if (!matches) {
     return Promise.resolve(input);
   }
 
-  const captured = YOUTUBE.exec(input);
+  for (var i = 0; i<matches.length; i++) {
+    matchID = matches[i].match(YOUTUBE)[1];
 
-  const matchID = captured[1];
+    if (!responseBody) {
+      responseBody = responseBody + input.replace(matches[i], `<iframe style="min-height: 350px" src="//www.youtube.com/embed/${matchID}" frameborder="0" allowfullscreen></iframe>`);
+    } else {
+      responseBody = responseBody.replace(matches[i], `<iframe style="min-height: 350px" src="//www.youtube.com/embed/${matchID}" frameborder="0" allowfullscreen></iframe>`);
+    }
 
-  return Promise.resolve(input.replace(YOUTUBE, `<iframe style="min-height: 350px" src="//www.youtube.com/embed/${matchID}" frameborder="0" allowfullscreen></iframe>`));
+    if (i === matches.length - 1) {
+      return Promise.resolve(responseBody);
+    }
+  }
 }
