4b357cef128249d7197ad02e7e2649eabe0f8b4d
Noah Chase <nchase@gmail.com>
2016-10-17 20:05:23 -0400

    carry through query params for youtube embeds

--- test/unit/embed/youtube.js
+++ test/unit/embed/youtube.js
@@ -7,7 +7,6 @@ var dedent = require('dedent');
 describe('youtube embeds', function() {
   it('correctly produces frames for basic youtube URLs', function(done) {
     embedYouTube('https://www.youtube.com/watch?v=RD_44xEEDjU').then(function(output) {
-      console.log('call done', output);
       var expectedOutput = `<iframe style="min-height: 350px" src="//www.youtube.com/embed/RD_44xEEDjU" frameborder="0" allowfullscreen></iframe>`;
 
       assert.equal(output, expectedOutput);
--- v2/app/components/oembed/oembed-youtube.js
+++ v2/app/components/oembed/oembed-youtube.js
@@ -1,8 +1,9 @@
 module.exports = function embedYouTube(input) {
-  const YOUTUBE = /(?:https?:\/\/)?(?:m\.)?(?:www\.)?(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
+  const YOUTUBE = /(?:https?:\/\/)?(?:m\.)?(?:www\.)?(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})(.+)?/i;
 
   var matches = input && input.match(new RegExp(YOUTUBE, 'g'));
   var matchID;
+  var matchParams;
   var responseBody = '';
 
   if (!matches) {
@@ -11,11 +12,16 @@ module.exports = function embedYouTube(input) {
 
   for (var i = 0; i<matches.length; i++) {
     matchID = matches[i].match(YOUTUBE)[1];
+    matchParams = '';
+
+    if (matches[i].match(YOUTUBE)[2]) {
+      matchParams = '?' + matches[i].match(YOUTUBE)[2];
+    }
 
     if (!responseBody) {
-      responseBody = responseBody + input.replace(matches[i], `<iframe style="min-height: 350px" src="//www.youtube.com/embed/${matchID}" frameborder="0" allowfullscreen></iframe>`);
+      responseBody = responseBody + input.replace(matches[i], `<iframe style="min-height: 350px" src="//www.youtube.com/embed/${matchID}${matchParams}" frameborder="0" allowfullscreen></iframe>`);
     } else {
-      responseBody = responseBody.replace(matches[i], `<iframe style="min-height: 350px" src="//www.youtube.com/embed/${matchID}" frameborder="0" allowfullscreen></iframe>`);
+      responseBody = responseBody.replace(matches[i], `<iframe style="min-height: 350px" src="//www.youtube.com/embed/${matchID}?${matchParams}" frameborder="0" allowfullscreen></iframe>`);
     }
 
     if (i === matches.length - 1) {
