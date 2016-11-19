2294213b73020f265bfe9676ef9fb6bdd3e2674e
Noah Chase <nchase@gmail.com>
2016-11-19 14:15:53 -0500

    only request permapost from server if we don't have it

--- v2/app/components/ShowPost.jsx
+++ v2/app/components/ShowPost.jsx
@@ -2,9 +2,20 @@ var React = require('react');
 var ThreadPost = require('./ThreadPost.jsx');
 var Loader = require('./Loader.jsx');
 var superagent = require('superagent');
+var _ = require('lodash/core');
 
 module.exports = React.createClass({
   componentWillMount: function() {
+    var thread = _.find(this.props.value.topics, {
+      id: parseInt(this.props.params.id, 10)
+    });
+
+    if (thread) {
+      return this.setState({
+        postData: thread
+      });
+    }
+
     superagent.get(`/topic/${parseInt(this.props.routeParams.id, 10)}`)
       .set('Accept', 'application/json')
       .then(function(response){
