5eb08d6a8ffb73701f10046154a19c26655fb83d
Noah Chase <nchase@gmail.com>
2016-12-18 15:41:12 -0500

    move socket handling to a separate module

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,6 @@ var PostsController = rootRequire('controllers/PostsController');
 var UserController =  rootRequire('controllers/UserController');
 var authentication =  rootRequire('lib/authentication');
 
-var userlist = {};
 var superagent = require('superagent');
 
 var routes = function(app, passport){
@@ -216,92 +215,6 @@ var routes = function(app, passport){
     //return post data using response.send which should return the markdown you need to quote the post
     PostsController.get(request.params.id).done(function(err, post){
       response.send(post);
-    });
-  });
-
-  // TODO: what follows isn't isn't a proper route,
-  // it's a socket/event binding.
-  // take some time to think about where this ought to live.
-  // probably in the PostsController module.
-  app.get('io').sockets.on('connection', function(socket){
-    if (typeof socket.handshake.query.user === "undefined") {
-      socket.handshake.query.user = "{}";
-    }
-
-    var user = JSON.parse(socket.handshake.query.user);
-
-    if (user.id && user.id !== 0) {
-      UserController.get(user.id).then(function(userData) {
-        if (!userData.hide_connected) {
-          userlist[user.name] = {
-            name: user.name
-          }
-        }
-
-        app.get('io').sockets.emit('updateuserlist', userlist);
-      });
-    }
-
-    socket.on('post', function(data, callback) {
-      data.user_id = data.user.id;
-
-      if (data.user_id === 0) {
-        return false;
-      }
-
-      data.created_at = new Date();
-
-      PostsController.add(data, function(error, result){
-        data.id = result.id;
-        app.get('io').sockets.emit('post', data);
-
-        UserController.updateLastVisited(data.user.id, data.parent);
-
-        app.set('lastModifiedIndex', new Date());
-
-        if (typeof callback === "function") {
-          callback();
-        }
-      });
-    });
-
-    socket.on('edit', function(data){
-      data.user_id = data.user.id;
-      app.get('io').sockets.emit('edit', data);
-
-      if (data.user_id === 0) {
-        return false;
-      }
-
-      PostsController.edit(data);
-    });
-
-    socket.on('destroy', function(data){
-      data.user_id = data.user.id;
-      app.get('io').sockets.emit('destroy', data);
-
-      if (data.user_id === 0) {
-        return false;
-      }
-
-      PostsController.destroy(data);
-    });
-
-    socket.on('markallread', function(data, callback) {
-      data.user_id = data.user.id;
-
-      if (data.user_id === 0) {
-        return false;
-      }
-
-      UserController.markAllRead(data);
-    });
-
-    socket.on('disconnect', function() {
-      if (user.name) {
-        delete userlist[user.name];
-        app.get('io').sockets.emit('updateuserlist', userlist);
-      }
     });
   });
 
