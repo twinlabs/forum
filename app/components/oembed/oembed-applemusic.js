d77eeeb1c119f6b464f46062137c15e2268b5472
Noah Chase <nchase@gmail.com>
2017-04-30 19:28:46 -0400

    allow playlist matching from apple music

--- app/components/oembed/oembed-applemusic.js
+++ app/components/oembed/oembed-applemusic.js
@@ -1,11 +1,19 @@
 module.exports = function embed(input) {
-  const MATCH = /(?:https?:\/\/)(itun.es|geo.itunes.apple.com)\/.*i=([0-9]+).*/;
+  const MATCH = /(?:https?:\/\/).*(itun\.es|itunes\.apple\.com)\/.*(.*i=([0-9]+)|playlist\/.*idpl\.([a-zA-Z0-9]+)).*/;
 
   if (!input || input.match(MATCH) === null) {
     return Promise.resolve(input);
   }
 
-  const [, , matchID] = MATCH.exec(input);
+  let [, , , matchID] = MATCH.exec(input);
+
+  if (!matchID) {
+    matchID = MATCH.exec(input)[4];
+  }
+
+  if (input.match('\/playlist\/')) {
+    return Promise.resolve(input.replace(MATCH, `<iframe src="https://tools.applemusic.com/embed/v1/playlist/pl.${matchID}" width="100%" height="500" frameborder="0" allowtransparency="true"></iframe>`));
+  }
 
   return Promise.resolve(input.replace(MATCH, `<iframe src="https://tools.applemusic.com/embed/v1/song/${matchID}" width="100%" height="110" frameborder="0" allowtransparency="true"></iframe>`));
 }
